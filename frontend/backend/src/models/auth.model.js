09a6d6ee4adaccc3ee62d3efbfab8f5ebf3949dd
vishalgothartwal <vishalgotharwal877@gmail.com>
2025-02-09 10:40:27 +0530

    backend

---
+++ frontend/backend/src/models/auth.model.js
@@ -0,0 +1,32 @@
+import mongoose, { Schema } from "mongoose"  
+
+
+const userSchema = new mongoose.Schema({
+
+    firstname : {
+        type :String , 
+        required : true , 
+    } , 
+    lastname : {
+         type :String , 
+         required : true ,  
+    } , 
+    email :  {
+          type : String , 
+          required : true , 
+    },
+    password : {
+       type : String , 
+       required : true ,    
+    } , 
+
+    //so here we cant store this because we do not want to store it  in databse 
+    // confirmPassword : {
+    //      type : String , 
+    //      required : true , 
+    // }
+
+
+
+
+})
